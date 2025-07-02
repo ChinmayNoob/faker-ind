3addf71227f3b3eeeac84dff8b77aaa762901ee5
Chirag Chhajed <chhajedchirag18@gmail.com>
2025-07-02 21:10:32 +0530

    Empty branch for code review

--- src/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { Language } from "./types/language";
-
-export const fakerIndOptsSchema = z.object({
-    language: z.nativeEnum(Language).optional(),
-}).strict();
-
-export type FakerIndOpts = z.infer<typeof fakerIndOptsSchema>;
